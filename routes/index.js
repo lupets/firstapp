e9a5d534dbd53dd3b7fb786777de7481555d922c
Peter Burgos <peter.burgos@lupet.se>
2020-05-16 18:08:35 +0200

    getting landing page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-
+let landing = require('../controllers/landing');
 let index = require('../controllers/index')
 
 /* GET home page. */
-router.get('/', index.index);
+router.get('/', landing.get_landing);
+router.get('/index', index.index);
 
 module.exports = router;
